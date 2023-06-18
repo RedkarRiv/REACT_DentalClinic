5b19297026125553cb43040227b09e37221b20f6
Carlos <carresan.val@gmail.com>
2023-06-19 00:07:23 +0200

    UPDATE: Ajustes minimos de diseño

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,28 @@ import { ContactFormCard } from "../../common/ContactFormCard/ContactFormCard";
 import { CallUsButton } from "../../common/CallUsButton/CallUsButton";
 import { TreatmentCard } from "../../common/TreatmentCard/TreatmentCard";
 import cirujiaMaxilar from "../../assets/img/cirujiaMaxilar.jpg"
+import ortodoncia from "../../assets/img/ortodoncia.jpg"
+import caries from "../../assets/img/caries.svg"
+import esteticaDental from "../../assets/img/esteticaDental.svg"
+import protesisDental from "../../assets/img/protesisDental.svg"
+import odontopediatria from "../../assets/img/odontopediatria.svg"
+import periodoncia from "../../assets/img/periodoncia.svg"
+import dolorMaxilar from "../../assets/img/dolorMaxilar.svg"
+import cirugiaOral from "../../assets/img/cirugiaOral.svg"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import { SectionTitle } from "../../common/SectionTitle/SectionTitle";
 import { RegisterFormCard } from "../../common/RegisterFormCard/RegisterFormCard";
 
@@ -17,7 +39,7 @@ export const Home = () => {
           <div className="slide12">
             <div className="slide121">
               <div className="contactFormCardHomeContainer">
-                <ContactFormCard titleContact={"Nosotros te llamamos"} />
+                <ContactFormCard titleContact={"CONSULTA GRATUITA"} />
               </div>
             </div>
             <div className="slide122">
@@ -39,16 +61,16 @@ export const Home = () => {
         <SectionTitle sectionTitle={"Nuestras especialidades"} />
         <div className="mainSpecialitiesCardsHomeContainer">
         <div className="specialitiesCardsHomeContainer">
-          <div className="specialityCardHomeContainer"><TreatmentCard id={1} img_link={cirujiaMaxilar} img_name={"cirujia_maxilar"}  treatmentTitle={"CIRUJÍA MÁXILAR"} treatmentDescription={"Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias "}/></div>
-          <div className="specialityCardHomeContainer"><TreatmentCard id={1} img_link={cirujiaMaxilar} img_name={"cirujia_maxilar"}  treatmentTitle={"CIRUJÍA MÁXILAR"} treatmentDescription={"Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias "}/></div>
-          <div className="specialityCardHomeContainer"><TreatmentCard id={1} img_link={cirujiaMaxilar} img_name={"cirujia_maxilar"}  treatmentTitle={"CIRUJÍA MÁXILAR"} treatmentDescription={"Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias "}/></div>
-          <div className="specialityCardHomeContainer"><TreatmentCard id={1} img_link={cirujiaMaxilar} img_name={"cirujia_maxilar"}  treatmentTitle={"CIRUJÍA MÁXILAR"} treatmentDescription={"Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias "}/></div>
+          <div className="specialityCardHomeContainer"><TreatmentCard id={1} img_link={ortodoncia} img_name={"ortoncia"}  treatmentTitle={"ORTODONCIA"} treatmentDescription={"Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias "}/></div>
+          <div className="specialityCardHomeContainer"><TreatmentCard id={1} img_link={caries} img_name={"caries"}  treatmentTitle={"CARIES"} treatmentDescription={"Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias "}/></div>
+          <div className="specialityCardHomeContainer"><TreatmentCard id={1} img_link={esteticaDental} img_name={"esteticaDental"}  treatmentTitle={"ESTÉTICA DENTAL"} treatmentDescription={"Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias "}/></div>
+          <div className="specialityCardHomeContainer"><TreatmentCard id={1} img_link={protesisDental} img_name={"protesisDental"}  treatmentTitle={"PRÓTESIS"} treatmentDescription={"Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias "}/></div>
 </div>
           <div className="specialitiesCardsHomeContainer">
-          <div className="specialityCardHomeContainer"><TreatmentCard id={1} img_link={cirujiaMaxilar} img_name={"cirujia_maxilar"}  treatmentTitle={"CIRUJÍA MÁXILAR"} treatmentDescription={"Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias "}/></div>
-          <div className="specialityCardHomeContainer"><TreatmentCard id={1} img_link={cirujiaMaxilar} img_name={"cirujia_maxilar"}  treatmentTitle={"CIRUJÍA MÁXILAR"} treatmentDescription={"Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias "}/></div>
-          <div className="specialityCardHomeContainer"><TreatmentCard id={1} img_link={cirujiaMaxilar} img_name={"cirujia_maxilar"}  treatmentTitle={"CIRUJÍA MÁXILAR"} treatmentDescription={"Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias "}/></div>
-          <div className="specialityCardHomeContainer"><TreatmentCard id={1} img_link={cirujiaMaxilar} img_name={"cirujia_maxilar"}  treatmentTitle={"CIRUJÍA MÁXILAR"} treatmentDescription={"Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias "}/></div>
+          <div className="specialityCardHomeContainer"><TreatmentCard id={1} img_link={odontopediatria} img_name={"odontopediatria"}  treatmentTitle={"ODONTOPEDIATRÍA"} treatmentDescription={"Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias "}/></div>
+          <div className="specialityCardHomeContainer"><TreatmentCard id={1} img_link={periodoncia} img_name={"periodoncia"}  treatmentTitle={"PERIODONCÍA"} treatmentDescription={"Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias "}/></div>
+          <div className="specialityCardHomeContainer"><TreatmentCard id={1} img_link={dolorMaxilar} img_name={"dolorMaxilar"}  treatmentTitle={"DOLOR Y ARTICULACIÓN"} treatmentDescription={"Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias "}/></div>
+          <div className="specialityCardHomeContainer"><TreatmentCard id={1} img_link={cirugiaOral} img_name={"cirugiaOral"}  treatmentTitle={"CIRUGÍA ORAL"} treatmentDescription={"Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias "}/></div>
         </div>
         </div>
       </div>
