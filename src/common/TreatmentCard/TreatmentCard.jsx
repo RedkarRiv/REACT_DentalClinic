2fe56b59f691895b776c74c2afeff19e4ea4cb7c
Carlos <carresan.val@gmail.com>
2023-06-18 22:15:33 +0200

    UPDATE:Creado componente TreatmenteCard Añadidos slide2 y slide3 en el Home. Slide2: Listado de especialidades. Slide3: Registro de usuarios

--- src/common/TreatmentCard/TreatmentCard.jsx
+++ src/common/TreatmentCard/TreatmentCard.jsx
@@ -5,7 +5,7 @@ export const TreatmentCard = ({id, img_name, img_link, treatmentTitle, treatment
   return (
     <div className="TreatmentCardDesign" id={id}>
       <div className="treatmentImage">
-      <img className="treatmentImageDesign" src= {img_link}  alt={img_name}></img>
+      <img className="treatmentImageDesign" src= {img_link}  alt={img_name} id={`image${id}`}></img>
 
       </div>
       <div className="treatmentText">
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import "./Home.css";
-import logo from "../../assets/img/logo-removebg-preview.png";
-
-import { OptionButton } from "../../common/OptionButton/OptionButton";
 import { StatCard } from "../../common/StatCard/StatCard";
 import { ContactFormCard } from "../../common/ContactFormCard/ContactFormCard";
 import { CallUsButton } from "../../common/CallUsButton/CallUsButton";
 import { TreatmentCard } from "../../common/TreatmentCard/TreatmentCard";
+import cirujiaMaxilar from "../../assets/img/cirujiaMaxilar.jpg"
+import { SectionTitle } from "../../common/SectionTitle/SectionTitle";
+import { RegisterFormCard } from "../../common/RegisterFormCard/RegisterFormCard";
+
 
 export const Home = () => {
   return (
@@ -19,7 +20,6 @@ export const Home = () => {
                 <ContactFormCard titleContact={"Nosotros te llamamos"} />
               </div>
             </div>
-            {/* name, data, id */}
             <div className="slide122">
               <div className="callUsHomeContainer">
                 <CallUsButton />
@@ -36,18 +36,27 @@ export const Home = () => {
         </div>
       </div>
       <div className="slide2">
+        <SectionTitle sectionTitle={"Nuestras especialidades"} />
         <div className="mainSpecialitiesCardsHomeContainer">
         <div className="specialitiesCardsHomeContainer">
-          <div className="specialityCardHomeContainer"><TreatmentCard id={1} img_lin={"https://dr-samir.com/wp-content/uploads/2018/06/cirugia-maxilofacial-cabecera.jpg"} img_name={"cirujia_maxilar"}  treatmentTitle={"CIRUJÍA MÁXILAR"} treatmentDescription={"Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias "}/></div>
-          <div className="specialityCardHomeContainer"></div>
-          <div className="specialityCardHomeContainer"></div>
-          <div className="specialityCardHomeContainer"></div></div>
+          <div className="specialityCardHomeContainer"><TreatmentCard id={1} img_link={cirujiaMaxilar} img_name={"cirujia_maxilar"}  treatmentTitle={"CIRUJÍA MÁXILAR"} treatmentDescription={"Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias "}/></div>
+          <div className="specialityCardHomeContainer"><TreatmentCard id={1} img_link={cirujiaMaxilar} img_name={"cirujia_maxilar"}  treatmentTitle={"CIRUJÍA MÁXILAR"} treatmentDescription={"Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias "}/></div>
+          <div className="specialityCardHomeContainer"><TreatmentCard id={1} img_link={cirujiaMaxilar} img_name={"cirujia_maxilar"}  treatmentTitle={"CIRUJÍA MÁXILAR"} treatmentDescription={"Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias "}/></div>
+          <div className="specialityCardHomeContainer"><TreatmentCard id={1} img_link={cirujiaMaxilar} img_name={"cirujia_maxilar"}  treatmentTitle={"CIRUJÍA MÁXILAR"} treatmentDescription={"Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias "}/></div>
+</div>
           <div className="specialitiesCardsHomeContainer">
-          <div className="specialityCardHomeContainer"></div>
-          <div className="specialityCardHomeContainer"></div>
-          <div className="specialityCardHomeContainer"></div>
-          <div className="specialityCardHomeContainer"></div>
+          <div className="specialityCardHomeContainer"><TreatmentCard id={1} img_link={cirujiaMaxilar} img_name={"cirujia_maxilar"}  treatmentTitle={"CIRUJÍA MÁXILAR"} treatmentDescription={"Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias "}/></div>
+          <div className="specialityCardHomeContainer"><TreatmentCard id={1} img_link={cirujiaMaxilar} img_name={"cirujia_maxilar"}  treatmentTitle={"CIRUJÍA MÁXILAR"} treatmentDescription={"Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias "}/></div>
+          <div className="specialityCardHomeContainer"><TreatmentCard id={1} img_link={cirujiaMaxilar} img_name={"cirujia_maxilar"}  treatmentTitle={"CIRUJÍA MÁXILAR"} treatmentDescription={"Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias "}/></div>
+          <div className="specialityCardHomeContainer"><TreatmentCard id={1} img_link={cirujiaMaxilar} img_name={"cirujia_maxilar"}  treatmentTitle={"CIRUJÍA MÁXILAR"} treatmentDescription={"Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias "}/></div>
         </div>
+        </div>
+      </div>
+      <div className="slide3">
+        <div className="slide31">
+        <SectionTitle sectionTitle={"¡PRIMERA CITA GRATIS!"} />
+          <div className="registerFormCardHomeDesign"><RegisterFormCard titleRegister={"REGISTRATE GRATIS"}/></div>
+          
         </div>
       </div>
     </div>
