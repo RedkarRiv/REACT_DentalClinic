fd384b269aeb4d126b24748468a22c2056627238
Carlos <carresan.val@gmail.com>
2023-06-20 21:59:50 +0200

    FIX: Eliminado Componente Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.css';
-import { Header } from "./common/Header/Header";
 import { Body } from "./pages/Body/Body";
 import { Footer } from "./common/Footer/Footer";
 import { NavbarMain } from "./common/Navbar/Navbar";
@@ -9,7 +8,6 @@ import { NavbarMain } from "./common/Navbar/Navbar";
 function App() {
   return (
     <>
-      {/* <Header/> */}
       <NavbarMain/>
       <Body/>
       <Footer/>
